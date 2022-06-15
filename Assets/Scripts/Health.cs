d2c35cad77a6578d56fa4937ef24fea49fca06b5
Patrick <Patrick@Moffett.dev>
2022-06-15 11:16:17 -0700

    Created Health Component

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Health : MonoBehaviour,IDamageable
+{
+    public event Action<float> OnCurrentHealthChanged;
+    public event Action<float> OnMaxHealthChanged;
+    public event Action OnHealthIsZero;
+    
+    [SerializeField]
+    private float maxHealth = 100f;
+    private float _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        Mathf.Clamp(_currentHealth, 0, maxHealth);
+        OnCurrentHealthChanged?.Invoke(_currentHealth);
+        if (_currentHealth == 0f)
+        {
+            OnHealthIsZero?.Invoke();
+        }
+    }
+
+    public void SetHealth(float newHealth)
+    {
+        _currentHealth = newHealth;
+        Mathf.Clamp(_currentHealth, 0, maxHealth);
+        OnCurrentHealthChanged?.Invoke(_currentHealth);
+        if (_currentHealth == 0f)
+        {
+            OnHealthIsZero?.Invoke();
+        }
+    }
+
+    public void AddHealth(float amountToAdd)
+    {
+        _currentHealth += amountToAdd;
+        Mathf.Clamp(_currentHealth, 0, maxHealth);
+        OnCurrentHealthChanged?.Invoke(_currentHealth);
+        if (_currentHealth == 0f)
+        {
+            OnHealthIsZero?.Invoke();
+            Debug.LogWarning(gameObject.name + " Health Component reached zero health after adding health.");
+        }
+    }
+
+    public float GetCurrentHealth()
+    {
+        return _currentHealth;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public void SetMaxHealth(float newMaxHealth)
+    {
+        maxHealth = newMaxHealth;
+        Mathf.Clamp(_currentHealth, 0, maxHealth);
+        OnMaxHealthChanged?.Invoke(newMaxHealth);
+    }
+
+    public float GetHealthPercentage()
+    {
+        return _currentHealth / maxHealth;
+    }
+}
