f39f1203e49b28618eaa8dfebb0c64d1e0877f4f
Patrick <Patrick@moffett.dev>
2022-08-02 13:30:06 -0700

    Created a Timer Switch that toggles switches at timed intervals

---
+++ Assets/Scripts/Interactables/Switches/TimerToggleSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Interactables.Switches
+{
+    public class TimerToggleSwitch : MonoBehaviour
+    {
+        public float initialDelay = 0f;
+        public float toggleTime = 1f;
+        public RespondsToSwitch switchableObject;
+
+        private Coroutine _timedSwitchCoroutine;
+        public void Start()
+        {
+            if (switchableObject != null)
+            {
+                _timedSwitchCoroutine = StartCoroutine(TimedSwitch());
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_timedSwitchCoroutine != null)
+            {
+                StopCoroutine(_timedSwitchCoroutine);
+            }
+        }
+
+        IEnumerator TimedSwitch()
+        {
+            yield return new WaitForSeconds(initialDelay);
+            while (true)
+            {
+                switchableObject.ToggleSwitch();
+                yield return new WaitForSeconds(toggleTime);
+            }
+        }
+    }
+}
