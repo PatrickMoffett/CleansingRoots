9a9aafd6bb6acf53bf5f6b8b8194e57bd9ebe3b2
Patrick <Patrick@moffett.dev>
2022-08-02 13:08:42 -0700

    Created Custom Inspector buttons to anything that responds to switches

---
+++ Assets/Scripts/Editor/CustomRespondsToSwitchInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(RespondsToSwitch),true)]
+public class CustomRespondsToSwitchInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        RespondsToSwitch switchable = (RespondsToSwitch)target;
+
+        if (GUILayout.Button("Switch On"))
+        {
+            switchable.SwitchOn();
+        }
+        if (GUILayout.Button("Switch Off"))
+        {
+            switchable.SwitchOff();
+        }
+        if (GUILayout.Button("Toggle Switch"))
+        {
+            switchable.ToggleSwitch();
+        }
+    }
+}
