79ad76321c0fb6bccd6024538c18544ad715b493
Gamkedo <chris@gamkedo.com>
2022-08-12 20:51:16 -0400

    Slingshot pebble hit randomizes which clip variation is used

---
+++ Assets/Scripts/Systems/AudioManager/SoundRandomize.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRandomize : MonoBehaviour
+{
+    public AudioClip[] randOptions;
+    void Start()
+    {
+        AudioSource mySource = GetComponent<AudioSource>();
+        mySource.clip = randOptions[Random.Range(0, randOptions.Length)];
+    }
+}
