b967af973f9bc3a60e0a449dabe4109c0d34402c
Patrick <Patrick@moffett.dev>
2022-08-27 11:43:09 -0700

    Added Boss Minion Spawner and Boss Robot Minion Prefab

---
+++ Assets/Scripts/Combat/Boss/BossSpawner.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class BossSpawner : MonoBehaviour
+{
+    [SerializeField]private GameObject enemyToSpawn;
+    [SerializeField]private int numberOfEnemiesToSpawn = 3;
+    [SerializeField] private float spawnDelay = 5f;
+    [SerializeField] private float rangeToSpawn = 10f;
+
+    private List<GameObject> _managedEnemies = new List<GameObject>();
+
+    private bool waitingToSpawn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < _managedEnemies.Count; i++)
+        {
+            if (_managedEnemies[i] == null)
+            {
+                _managedEnemies.RemoveAt(i);
+            }
+        }
+
+        if (_managedEnemies.Count == 0 && !waitingToSpawn)
+        {
+            StartCoroutine(SpawnEnemies());
+        }
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        waitingToSpawn = true;
+        yield return new WaitForSeconds(spawnDelay);
+        for (int i = 0; i < numberOfEnemiesToSpawn; i++)
+        {
+            Vector3 spawnPosition = Random.insideUnitSphere * rangeToSpawn;
+            spawnPosition.y = 0;
+            spawnPosition += transform.position;
+            GameObject newEnemy = Instantiate(enemyToSpawn, spawnPosition, Quaternion.identity);
+            _managedEnemies.Add(newEnemy);
+        }
+
+        waitingToSpawn = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position,rangeToSpawn);
+    }
+}
---
+++ Assets/Scripts/Enemies/FlyingRobot/BossMinionBehaviorTree.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using AI.BehaviorTree;
+using AI.BehaviorTree.Control.Decorator;
+using AI.BehaviorTree.Node.Control.Composite;
+using AI.BehaviorTree.Node.Task;
+using AI.BehaviorTree.Service;
+using AI.BehaviorTree.Task;
+using AI.WaypointNavigation;
+using Enemies.AttackComponents;
+using UnityEngine;
+
+namespace Enemies.FlyingRobot
+{
+    public class BossMinionBehaviorTree : BehaviorTree
+    {
+        public float moveSpeed = 5f;
+        public float aggroRange = 40f;
+        public float attackDistance = 30f;
+        public float patrolDistanceTolerance = .2f;
+        public float navigationUpdateRate = 1f;
+        private GameObject _playerGameObject;
+
+        private readonly string _selfKey = "Self";
+        private readonly string _playerKey = "Player";
+        private readonly string _moveSpeedKey = "MoveSpeed";
+        private readonly string _aggroRangeKey = "AggroRange";
+        private readonly string _attackDistanceKey = "AttackDistance";
+        private readonly string _patrolMinimumDistanceKey = "PatrolMinimumDistance";
+        private readonly string _waypointIndexKey = "WaypointIndexKey";
+        private readonly string _targetWaypointKey = "TargetWaypoint";
+        private readonly string _navPathKey = "NavPathList";
+        private readonly string _attackComponentKey = "AttackComponent";
+
+        private static GameObject _player;
+
+        protected override BaseNode SetupTree()
+        {
+            if (_playerGameObject == null)
+            {
+                _playerGameObject = GameObject.Find("Player");
+            }
+            
+            BaseAttackComponent attackComponent = GetComponent<BaseAttackComponent>();
+            SetData(_attackComponentKey,attackComponent);
+            SetData(_waypointIndexKey,0);
+            SetData(_selfKey,gameObject);
+            SetData(_playerKey, _playerGameObject);
+            SetData(_moveSpeedKey, moveSpeed);
+            SetData(_aggroRangeKey, aggroRange);
+            SetData(_attackDistanceKey, attackDistance);
+            SetData(_patrolMinimumDistanceKey,patrolDistanceTolerance);
+            return new Selector(new List<BaseNode>
+            {
+
+                new SetPathToPlayerService(_selfKey,_targetWaypointKey,_waypointIndexKey,_navPathKey,navigationUpdateRate,
+                        new Selector(new List<BaseNode>{
+                                    new CurrentWaypointIsPlayer(_targetWaypointKey,AbortType.BOTH,
+                                        new Sequence(new List<BaseNode> {
+                                                    new GameObjectWithinDistance(_attackDistanceKey,_selfKey,_playerKey,AbortType.BOTH,
+                                                        new RepeatUntilFail(
+                                                            new Sequence(new List<BaseNode> {
+                                                                        new FaceTargetWaypoint(_selfKey,_targetWaypointKey),
+                                                                        new PerformAttack(_attackComponentKey),
+                                                                        new IdleTask(1f)
+                                                                })
+                                                            )
+                                                        ),
+                                                    new MoveToWaypoint(_selfKey,_moveSpeedKey,_patrolMinimumDistanceKey,_targetWaypointKey),
+                                            })
+                                        ),
+                                    new Sequence(new List<BaseNode> {
+                                        new MoveToWaypoint(_selfKey, _moveSpeedKey,_patrolMinimumDistanceKey,_targetWaypointKey),
+                                        new SetNextWaypointFromList(_navPathKey,_waypointIndexKey,_targetWaypointKey)
+                                    })
+                                })
+                    )
+            });
+        }
+
+        private void OnDrawGizmos()
+        {
+
+            //draw nav to player path
+            Gizmos.color = Color.green;
+            List<BaseNavigationNode> path = (List<BaseNavigationNode>)GetData(_navPathKey);
+            if (path != null)
+            {
+                for (int i = 1; i < path.Count; i++)
+                {
+                    Gizmos.DrawLine(path[i-1].transform.position, path[i].transform.position);
+                }
+            }
+            
+            //draw current Target Node path
+            Gizmos.color = Color.magenta;
+            BaseNavigationNode targetNode = (BaseNavigationNode)GetData(_targetWaypointKey);
+            if (targetNode != null)
+            {
+                Gizmos.DrawLine(transform.position,targetNode.transform.position);
+            }
+        }
+    }
+}
