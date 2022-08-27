9c5eab682d1aadab93a4df991dda45fbf9a6859a
Patrick <Patrick@moffett.dev>
2022-08-27 11:18:30 -0700

    Audio Manager Fix

--- Assets/Scripts/Systems/AudioManager/AudioManager.cs
+++ Assets/Scripts/Systems/AudioManager/AudioManager.cs
@@ -4,14 +4,6 @@ using UnityEngine.Audio;
 
 namespace Systems.AudioManager
 {
-    [CreateAssetMenu(menuName="ScriptableObject/Settings/AudioManagerParams")]
-    public class AudioManagerParams : ScriptableObject
-    {
-        public AudioMixer mixer;
-        public AudioMixerGroup masterGroup;
-        public AudioMixerGroup sfxGroup;
-        public AudioMixerGroup musicGroup;
-    }
     public class AudioManager : IService
     {
         //params set from the editor
---
+++ Assets/Scripts/Systems/AudioManager/AudioManagerParams.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Systems.AudioManager
+{
+    [CreateAssetMenu(menuName="ScriptableObject/Settings/AudioManagerParams")]
+    public class AudioManagerParams : ScriptableObject
+    {
+        public AudioMixer mixer;
+        public AudioMixerGroup masterGroup;
+        public AudioMixerGroup sfxGroup;
+        public AudioMixerGroup musicGroup;
+    }
+}
