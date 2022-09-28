cefe8cd2d7940c269a54c92d68efd7cbdf4640a7
Patrick <Patrick@moffett.dev>
2022-09-27 19:50:57 -0700

    Added Enemy Robot Thruster Particle Effect

--- Assets/Scripts/AI/BehaviorTree/Node/Task/FaceTargetWaypoint.cs
+++ Assets/Scripts/AI/BehaviorTree/Node/Task/FaceTargetWaypoint.cs
@@ -7,11 +7,14 @@ namespace AI.BehaviorTree.Node.Task
     {
         private readonly string _selfGameObjectKey;
         private readonly string _targetWaypointKey;
+        private readonly string _turnSpeedKey;
+        private const float Tolerance = .01f;
 
-        public FaceTargetWaypoint(string selfGameObjectKey,string targetWaypointKey)
+        public FaceTargetWaypoint(string selfGameObjectKey,string targetWaypointKey, string turnSpeedKey)
         {
             _selfGameObjectKey = selfGameObjectKey;
             _targetWaypointKey = targetWaypointKey;
+            _turnSpeedKey = turnSpeedKey;
         }
         public override NodeState Run()
         {
@@ -21,11 +24,29 @@ namespace AI.BehaviorTree.Node.Task
                 return NodeState.FAILURE;
             }
 
+            //get all data
             BaseNavigationNode targetNode = (BaseNavigationNode)owningTree.GetData(_targetWaypointKey);
             GameObject self = (GameObject)owningTree.GetData(_selfGameObjectKey);
+            float turnSpeed = (float)owningTree.GetData(_turnSpeedKey);
+            
             Vector3 direction = targetNode.gameObject.transform.position - self.transform.position;
-            self.transform.forward = direction;
-            return NodeState.SUCCESS;
+            float dot = Vector3.Dot(self.transform.right, direction);
+            if (dot > -Tolerance && dot < Tolerance)
+            {
+                self.transform.forward = direction;
+                return NodeState.SUCCESS;
+            }
+
+            if (dot > 0)
+            {
+                self.transform.forward = Quaternion.AngleAxis(turnSpeed * Time.deltaTime, Vector3.up)*self.transform.forward;
+            }
+            else
+            {
+                self.transform.forward = Quaternion.AngleAxis(turnSpeed * Time.deltaTime, -Vector3.up)*self.transform.forward;
+            }
+
+            return NodeState.RUNNING;
         }
 
         public override void Reset()
