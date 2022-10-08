651e18141c88ab76a59b9d0a98b3fa90a0435879
Patrick <Patrick@moffett.dev>
2022-10-08 13:28:47 -0700

    Added Breakable Barrels that drop pickups

---
+++ Assets/Scripts/General/ExplosionForceChildren.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General
+{
+    public class ExplosionForceChildren : MonoBehaviour
+    {
+        public float force = 1f;
+        public float explosionRadius = 1f;
+        public Vector3 explosionOffset = Vector3.zero;
+        
+        
+        private void Start()
+        {
+            Rigidbody[] rbs = GetComponentsInChildren<Rigidbody>();
+
+            for (int i = 0; i < rbs.Length; i++)
+            {
+                rbs[i].AddExplosionForce(force,transform.position + explosionOffset,explosionRadius);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/Misc/DestructableContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Interactables.Misc
+{
+    public class DestructableContainer : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private List<GameObject> drops = new List<GameObject>();
+        [SerializeField] private float dropChance = .5f;
+        [SerializeField] private Vector3 dropSpawnOffset = Vector3.zero;
+        [SerializeField] private GameObject BrokenVersion;
+        [SerializeField] private GameObject VFX;
+        [SerializeField] private Vector3 VFXOffset = Vector3.zero;
+        public void TakeDamage(int damage)
+        {
+            Destroy(gameObject);
+            
+            //Spawn Drop
+            if (drops.Count > 0 && Random.value <= dropChance)
+            {
+                int index = Random.Range(0, drops.Count);
+                Instantiate(drops[index], transform.position+ dropSpawnOffset, Quaternion.identity);
+            }
+
+            if (VFX != null)
+            {
+                Instantiate(VFX, transform.position + VFXOffset, Quaternion.identity);
+            }
+
+            if (BrokenVersion != null)
+            {
+                Instantiate(BrokenVersion, transform.position, transform.rotation);
+            }
+        }
+    }
+}
